9715ab46fe72a9dbe27ccffff6552ff97027f10f
masmelacarol <carolstefannym@gmail.com>
2020-03-03 15:34:18 -0500

    Se crean algunos reducer para la funcionalidad de la aplicación

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,32 +1,68 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
-import '../assets/styles/components/Login.scss'
-import googleIcon from '../assets/static/logo-google.png'
-import twitterIcon from '../assets/static/logo-twitter.png'
+import {loginRequest} from '../actions';
+import '../assets/styles/components/Login.scss';
+import googleIcon from '../assets/static/logo-google.png';
+import twitterIcon from '../assets/static/logo-twitter.png';
 
-const Login = () => (
-    <section className="login">
-        <section className="login__container">
-            <h2>Inicia Sesión</h2>
-            <form className="login__container--form">
-                <input type="text" className="input form" placeholder="Correo"/>
-                <input type="password" className="input form" placeholder="Contraseña"/>
-                <button className="button">Iniciar Sesión</button>
-                <div className="login__container--remember-me">
-                    <label>
-                        <input type="checkbox" name="" id="remember" value="check"/> Recuérdame
-                    </label>
-                    <a href="/">Olvidé mi contraseña</a>
-                </div>
-            </form>
-            <section className="login__container--social-media">
-                <div><img src={googleIcon} className="icon" alt="Logo de Google"/> Inicia sesión con Google</div>
-                <div><img src={twitterIcon} className="icon" alt="Logo de Twitter"/> Inicia sesión con Twitter</div>
+const Login = props => {
+    const [form, setValues] = useState({
+        email: '',
+    });
+const handleInput = event => {
+    setValues({
+        ...form,
+        [event.target.name]: event.target.value
+    })
+}
+const handleSubmit = event => {
+    event.preventDefault();
+    props.loginRequest(form);
+    props.history.push('/');
+}
+
+
+    return (
+        <section className="login">
+            <section className="login__container">
+                <h2>Inicia Sesión</h2>
+                <form className="login__container--form" onSubmit={handleSubmit}>
+                    <input
+                        name= 'email'
+                        type="email" 
+                        className="input form" 
+                        placeholder="Correo"
+                        onChange = {handleInput}
+                    />
+                    <input 
+                        name="password" 
+                        type="password" 
+                        className="input form" 
+                        placeholder="Contraseña"
+                        onChange = {handleInput}
+                    />
+                    <button className="button">Iniciar Sesión</button>
+                    <div className="login__container--remember-me">
+                        <label>
+                            <input type="checkbox" name="" id="remember" value="check"/> Recuérdame
+                        </label>
+                        <a href="/">Olvidé mi contraseña</a>
+                    </div>
+                </form>
+                <section className="login__container--social-media">
+                    <div><img src={googleIcon} className="icon" alt="Logo de Google"/> Inicia sesión con Google</div>
+                    <div><img src={twitterIcon} className="icon" alt="Logo de Twitter"/> Inicia sesión con Twitter</div>
+                </section>
+                <p className="login__container--register">No tienes ninguna cuenta 
+                    <Link to="/register">Registrate</Link></p>
             </section>
-            <p className="login__container--register">No tienes ninguna cuenta 
-                <Link to="/register">Registrate</Link></p>
         </section>
-    </section>
-);
+    );
+}
 
-export default Login;
+const mapDispatchToProps = {
+    loginRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
