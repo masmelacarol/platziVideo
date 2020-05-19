a9d8a3aa3a00eeb35bad703f86a693f09c453dbd
masmelacarol <carolstefannym@gmail.com>
2020-05-19 12:49:08 -0500

    new: se crea estructura de servidor para la aplicación

---
+++ src/server/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import webpack from 'webpack';
+
+dotenv.config();
+
+const { ENV, PORT } = process.env;
+const app = express();
+
+if (ENV === 'development') {
+    console.log('Development config');
+    const webpackConfig = require('../../webpack.config');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware = require('webpack-hot-middleware');
+    const compiler = webpack(webpackConfig);
+    const webpackServerConfig = {
+        port: PORT,
+        hot: true,
+    };
+
+    app.use(webpackDevMiddleware(compiler, webpackServerConfig));
+    app.use(webpackHotMiddleware(compiler));
+}
+
+app.get('*', (req, res) => {
+    res.send({
+        hello: 'express',
+    });
+});
+
+app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    else console.log(`server running in mode ${ENV} on port ${PORT}`);
+});
